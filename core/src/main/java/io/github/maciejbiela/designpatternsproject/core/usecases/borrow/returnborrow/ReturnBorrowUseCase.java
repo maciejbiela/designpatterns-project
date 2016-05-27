5fb1c7371c39252f19d5114ce54b457f6852b756
Maciej Biela <maciej.biela@icloud.com>
2016-05-27 22:34:12 +0200

    Delete return borrow use case implementation

--- core/src/main/java/io/github/maciejbiela/designpatternsproject/core/usecases/borrow/returnborrow/ReturnBorrowUseCase.java
+++ core/src/main/java/io/github/maciejbiela/designpatternsproject/core/usecases/borrow/returnborrow/ReturnBorrowUseCase.java
@@ -1,12 +1,8 @@
 package io.github.maciejbiela.designpatternsproject.core.usecases.borrow.returnborrow;
 
 import io.github.maciejbiela.designpatternsproject.core.model.Borrow;
-import io.github.maciejbiela.designpatternsproject.core.model.Item;
 import io.github.maciejbiela.designpatternsproject.core.repositories.borrow.BorrowsRepository;
 import io.github.maciejbiela.designpatternsproject.core.repositories.items.ItemsRepository;
-
-import java.time.LocalDate;
-import java.util.List;
 
 public class ReturnBorrowUseCase {
     private final BorrowsRepository borrowsRepository;
@@ -17,14 +13,6 @@ public class ReturnBorrowUseCase {
         this.itemsRepository = itemsRepository;
     }
 
-    public void returnBorrow(Borrow borrow, ReturnBorrowResponseHandler returnBorrowResponseHandler) {
-        final List<Item> items = borrow.getItems();
-        for (Item item : items) {
-            item.setAvailable(true);
-            itemsRepository.update(item);
-        }
-        borrow.setReturnDate(LocalDate.now());
-        borrowsRepository.update(borrow);
-        returnBorrowResponseHandler.onResponse(borrow);
+    public void returnBorrow(Borrow borrow) {
     }
 }
